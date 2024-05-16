54562e3c9e055f15e28bcc7e71f30983da640b1c
Chris TF <81719250+chris1234345@users.noreply.github.com>
2024-05-17 00:11:27 +0300

    implement complete layout

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyBannks = () => {
+  return (
+    <div>MyBannks</div>
+  )
+}
+
+export default MyBannks
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBlance from '@/components/TotalBlance'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Chris', lastName: 'TF', email: 'cristitf123@gmail.com'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext ="Access and manage your account and  transactions efficiently"
+          />
+          <TotalBlance 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+      user={loggedIn}
+      transaction={[]}
+      banks={[{currentBalance: 123.50}, {currentBalance: 500}]}
+      />
+    </section>
   )
 }
 
